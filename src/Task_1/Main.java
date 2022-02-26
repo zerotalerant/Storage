1419e38e337b0ff4c3377f1438f4894acf48b7bb
zerotalerant <hasa32892@gmail.com>
2022-02-26 10:42:58 +0600

    Task 1

---
+++ src/Task_1/Main.java
@@ -0,0 +1,10 @@
+package Task_1;
+
+public class Main {
+    public static void main ( String[] args )
+    {
+        Dublicate dublicates = new Dublicate ();
+        System.out.println ( dublicates.numbers () );
+    }
+}
+
