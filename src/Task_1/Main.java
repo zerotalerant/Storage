8c03c6c16c606a174cd4f0a6e13890ec1ed49b54
zerotalerant <hasa32892@gmail.com>
2022-02-26 14:20:15 +0600

    end it

--- src/Task_1/Main.java
+++ src/Task_1/Main.java
@@ -1,10 +1,23 @@
 package Task_1;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
-    public static void main ( String[] args )
-    {
-        Dublicate dublicates = new Dublicate ();
-        System.out.println ( dublicates.numbers () );
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите набор чисел: ");
+        Set<Character> set = new HashSet<>();
+        String numbers = scanner.next();
+
+
+        for (int i = 0; i < numbers.length(); i++) {
+            if (numbers.charAt(i) == ',') {
+                continue;
+            }
+            set.add(numbers.charAt(i));
+        }
+        System.out.println(set);
     }
 }
-
